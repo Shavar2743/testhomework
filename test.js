aa8f852f9afd37cc5c9a3e05f9f1c7d5d90ce368
Shavar <markus.shavar@web.de>
2024-04-30 14:36:24 +0200

    add data

---
+++ test.js
@@ -0,0 +1,23 @@
+function altersrechner() {
+    let alter = InputDeviceInfo
+    if(true){
+        alter < 6;
+        console.log(Die Fahrt ist Kostenlos)
+    }
+    if(true){
+        alter <;
+        console.log(Die Fahrt ist Kostenlos)
+    }
+    if(true){
+        alter < 6;
+        console.log(Die Fahrt ist Kostenlos)
+    }
+    if(true){
+        alter < 6;
+        console.log(Die Fahrt ist Kostenlos)
+    }
+    if(true){
+        alter < 6;
+        console.log(Die Fahrt ist Kostenlos)
+    }
+}
